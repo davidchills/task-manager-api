67f38563958c1ec7b0f50c65c3c2d7692e033559
Dave Hills <daveh@dailyhills.com>
2021-09-06 15:12:27 -0700

    Fixed main task router
    Changed passed in options are handled and added to the populate options.

--- src/routers/task.js
+++ src/routers/task.js
@@ -15,45 +15,59 @@ router.post('/tasks', auth, async (req, res) => {
 	try {
 		await task.save()
 		res.status(201).send(task)
-	} catch (error) {
-		res.status(400).send(error)
-	}
+	} 
+	catch (error) { res.status(400).send(error) }
 })
 
-// Get all 
+// Get all tasks with optional filtering and pagination
 // /tasks?completed=true
 // /tasks?limit=10&skip=0
 // /tasks?sortBy=createdAt_asc
 router.get('/tasks', auth, async (req, res) => {
-	const match = {}
-	const sort = {}
+	// const match = {}
+	// const sort = {}
 	
+	// The very basics of the needed options for populate
+	const populateOptions = { path: 'tasks' }
+
+	// Only add match criteria if suuplied
 	if (req.query.completed) {
-		match.completed = (req.query.completed === 'true')
+		populateOptions.match = {}
+		populateOptions.match.completed = (req.query.completed === 'true')
 	}
 
+	// Only add the sort data if it is supplied
 	if (req.query.sortBy) {
 		const parts = req.query.sortBy.split('_')
-		sort[parts[0]] = (parts[1] === 'desc') ? -1 : 1
+		populateOptions.sort = {}
+		populateOptions.sort[parts[0]] = (parts[1] === 'desc') ? -1 : 1
 	}
 
+	// Only add the limit if a value is supplied
+	if (req.query.limit) { populateOptions.limit = parseInt(req.query.limit, 10) }
+
+	// Only add the skip if a value is provided
+	if (req.query.skip) { populateOptions.skip = parseInt(req.query.skip, 10) }
+	// console.log(populateOptions)
+
 	try {
+		// A different way to get tasks for the authenticated user
 		// const tasks = await Task.find({ owner: req.user._id })
 		// res.send(tasks)
-		// Second way to get tasks for the authenticated user
-		await req.user.populate([{
-			path: 'tasks',
-			match: match,
-			options: {
-				limit: parseInt(req.query.limit, 10),
-				skip: parseInt(req.query.skip, 10),
-				sort: sort
-			}
-		}])
+		
+		// await req.user.populate([{
+		// 	path: 'tasks',
+		// 	match: match,
+		// 	options: {
+		// 		limit: parseInt(req.query.limit, 10),
+		// 		skip: parseInt(req.query.skip, 10),
+		// 		sort: sort
+		// 	}
+		// }])
+		await req.user.populate([populateOptions])
 		res.send(req.user.tasks)
-	} catch (error) {
-		res.status(400).send(error)
-	}
+	} 
+	catch (error) { res.status(500).send(error) }
 })
 
 // Get task by id
@@ -64,14 +78,10 @@ router.get('/tasks/:id', auth, async (req, res) => {
 
 	try {
 		const task = await Task.findOne({ _id, owner: req.user._id })
-		if (!task) {
-			res.status(404).send({ error: 'Task not found' })
-		}
+		if (!task) { res.status(404).send({ error: 'Task not found' }) }
 		res.send(task)
-	} catch (error) {
-		res.status.toString(500).send(error)
-	}
-	//console.log(req.params)
+	} 
+	catch (error) { res.status.toString(500).send(error) }
 })
 
 router.patch('/tasks/:id', auth, async (req, res) => {
@@ -82,25 +92,18 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 	const allowedUpdates = ['description', 'completed']
 	const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-	if (!isValidOperation) {
-		return res.status(400).send({ error: 'Invalid Updates!' })
-	}
+	if (!isValidOperation) { return res.status(400).send({ error: 'Invalid Updates!' }) }
 
 	try {
 		const task = await Task.findOne({ _id, owner: req.user._id })
 
-		if (!task) {
-			return res.status(404).send({ error: 'Task not found' })
-		}
+		if (!task) { return res.status(404).send({ error: 'Task not found' }) }
 
-		updates.forEach((update) => {
-			task[update] = req.body[update]
-		})
+		updates.forEach((update) => { task[update] = req.body[update] })
 		await task.save()
 		res.send(task)
-	} catch (error) {
-		res.status(500).send(error)
-	}	
+	} 
+	catch (error) { res.status(500).send(error) }	
 })
 
 router.delete('/tasks/:id', auth, async (req, res) => {
@@ -110,15 +113,10 @@ router.delete('/tasks/:id', auth, async (req, res) => {
 
 	try {
 		const task = await Task.findOneAndDelete({ _id, owner: req.user._id })
-
-		if (!task) {
-			return res.status(404).send({ error: 'Task not found' })
-		}
+		if (!task) { return res.status(404).send({ error: 'Task not found' }) }
 		res.send(task)
-
-	} catch (error) {
-		res.status(500).send(error)
-	}
+	} 
+	catch (error) { res.status(500).send(error) }
 })
 
 module.exports = router
